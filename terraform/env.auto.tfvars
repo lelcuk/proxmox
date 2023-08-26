44f87bf84d001abcb342cfe5479b297285fc1240
Alon Lelcuk <lelcuk@PCALON.lelcukh.com>
2023-08-26 10:56:37 +0300

    adding terraform directory

---
+++ terraform/env.auto.tfvars
@@ -0,0 +1,3 @@
+proxmox_api_url = "https://pve.lelcukh.com:8006/api2/json"  # Your Proxmox IP Address
+proxmox_api_token_id = "root@pam!terraform"  # API Token ID
+proxmox_api_token_secret = "f67f0b50-5fcc-4b32-80e4-88bede9fce7c"
---
+++ terraform/simple_vm.tf
@@ -0,0 +1,45 @@
+# Proxmox new VM from iso
+# https://registry.terraform.io/providers/Telmate/proxmox/latest/docs/resources/vm_qemu
+# ---
+
+resource "proxmox_vm_qemu" "flatcar-vm" {
+    
+    # VM General Settings
+    target_node = "pve2"
+    vmid        = "1601"
+    name        = "tera-test"
+    desc        = "just a test"
+    iso         = "local:iso/flatcar_production_iso_image.iso"
+    bios        = "seabios"
+    onboot      = false 
+    tablet      = false
+    agent       = 0
+
+    # VM CPU Settings
+    cores   = 4
+    sockets = 1
+    cpu     = "host"    
+    memory  = 4096
+    balloon = 1
+
+    # VM Cloud-Init Settings
+    os_type = "cloud-init"
+
+    # VM Network Settings
+    network {
+        bridge = "vmbr100"
+        model  = "virtio"
+    }
+
+    disk {
+        type    = "scsi"
+        storage = "local-lvm"
+        size    = "40G"
+    }
+
+    serial {
+        id   = 0
+        type = "socket"
+    }
+
+}
