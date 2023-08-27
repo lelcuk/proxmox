a39286316a72ae2be52aa4ea8d36bce45c4ecaa9
Alon Lelcuk <lelcuk@PCALON.lelcukh.com>
2023-08-27 16:59:41 +0300

    working cluster creation, fixed ballooning issues that cased boot to crush due to LMO

--- flatcar/README.md
+++ flatcar/README.md
@@ -1,2 +1,6 @@
 running
-`docker run --rm -i quay.io/coreos/butane:latest < file.yml > file.json`
+```bash
+docker run --rm -i quay.io/coreos/butane:latest < file.yml > file.json
+docker run --rm -i -v "${PWD}":/pwd --workdir /pwd quay.io/coreos/butane:latest -d /pwd < file.yml > file.ign
+```
+
--- terraform/simple_vm.tf
+++ terraform/simple_vm.tf
@@ -8,11 +8,11 @@ variable "vm_config_list" {
     description = "cluster to create."
     type        = map(any)
     default = {
-      #  kube-cp-1     = { zone = "pve",  vm_id = "1601"},
-      #  kube-cp-2     = { zone = "pve",  vm_id = "1602"},
-      #  kube-cp-3     = { zone = "pve2", vm_id = "2601"},
-        kube-worker-1 = { zone = "pve",  vm_id = "1603"},
-        kube-worker-2 = { zone = "pve2", vm_id = "2602"}
+      kube-cp-1     = { zone = "pve",  vm_id = "1601", base_vm = "1600", mac = "c6:eb:29:0d:7e:76" },
+      kube-cp-2     = { zone = "pve",  vm_id = "1602", base_vm = "1600", mac = "9a:42:88:f8:f0:43" },
+      kube-cp-3     = { zone = "pve2", vm_id = "2601", base_vm = "2600", mac = "ce:09:40:9b:bf:08" },
+      kube-worker-1 = { zone = "pve",  vm_id = "1603", base_vm = "1600", mac = "92:BC:F8:A4:AD:32" },
+      kube-worker-2 = { zone = "pve2", vm_id = "2602", base_vm = "2600", mac = "22:4B:BF:64:7A:D2" }
     }
 }
 
@@ -23,12 +23,13 @@ resource "proxmox_vm_qemu" "flatcar-vm" {
     target_node = each.value.zone
     vmid        = each.value.vm_id
     name        = each.key
+    clone       = "flatcar-base" #each.value.base_vm
     desc        = "just a test"
-    iso         = "local:iso/flatcar_production_iso_image.iso"
+    #iso         = "local:iso/flatcar_production_iso_image.iso"
     bios        = "seabios"
     onboot      = false 
     tablet      = false
-    agent       = 0
+    agent       = 1
     
 
     # VM CPU Settings
@@ -36,15 +37,16 @@ resource "proxmox_vm_qemu" "flatcar-vm" {
     sockets = 1
     cpu     = "host"    
     memory  = 4096
-    balloon = 1
+    balloon = 2048
 
     # VM Cloud-Init Settings
     os_type = "cloud-init"
 
     # VM Network Settings
     network {
-        bridge = "vmbr100"
-        model  = "virtio"
+        macaddr = each.value.mac
+        bridge  = "vmbr100"
+        model   = "virtio"
     }
 
     disk {
