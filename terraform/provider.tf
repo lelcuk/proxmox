4212fa80b8d3709ed5cd11f0e692aef22c29082c
Alon Lelcuk <lelcuk@PCALON.lelcukh.com>
2023-08-27 10:41:30 +0300

    Working very simple vm creation

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -31,12 +31,8 @@ variable "vm_ssh_key" {
 }
 
 provider "proxmox" {
-
     pm_api_url = var.proxmox_api_url
     pm_api_token_id = var.proxmox_api_token_id
     pm_api_token_secret = var.proxmox_api_token_secret
-
-    # (Optional) Skip TLS Verification
     pm_tls_insecure = true
-
 }
--- terraform/simple_vm.tf
+++ terraform/simple_vm.tf
@@ -1,19 +1,35 @@
 # Proxmox new VM from iso
 # https://registry.terraform.io/providers/Telmate/proxmox/latest/docs/resources/vm_qemu
 # ---
+#
+#
+# all the VMs we want to create
+variable "vm_config_list" {
+    description = "cluster to create."
+    type        = map(any)
+    default = {
+      #  kube-cp-1     = { zone = "pve",  vm_id = "1601"},
+      #  kube-cp-2     = { zone = "pve",  vm_id = "1602"},
+      #  kube-cp-3     = { zone = "pve2", vm_id = "2601"},
+        kube-worker-1 = { zone = "pve",  vm_id = "1603"},
+        kube-worker-2 = { zone = "pve2", vm_id = "2602"}
+    }
+}
 
 resource "proxmox_vm_qemu" "flatcar-vm" {
+    for_each = var.vm_config_list 
     
     # VM General Settings
-    target_node = "pve2"
-    vmid        = "1601"
-    name        = "tera-test"
+    target_node = each.value.zone
+    vmid        = each.value.vm_id
+    name        = each.key
     desc        = "just a test"
     iso         = "local:iso/flatcar_production_iso_image.iso"
     bios        = "seabios"
     onboot      = false 
     tablet      = false
     agent       = 0
+    
 
     # VM CPU Settings
     cores   = 4
@@ -37,10 +53,11 @@ resource "proxmox_vm_qemu" "flatcar-vm" {
         size    = "40G"
     }
 
-    serial {
-        id   = 0
-        type = "socket"
-    }
+    # Serial is not enabled in flatcar
+    #serial {
+    #    id   = 0
+    #    type = "socket"
+    #}
 
     #    ipconfig0 = "ip=10.10.20.15/24,gw=10.10.10.1"
     #    sshkeys = <<EOF
